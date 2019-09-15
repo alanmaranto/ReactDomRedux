2e276879f9a13b3e54b02ba96dfa98213752408d
alanmaranto <alanarostegui@hotmail.com>
2019-09-14 22:22:35 -0500

    added searchbar logical and sass

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,38 +3,50 @@ const reducer = (state, action) => {
     case 'SET_FAVORITE':
       return {
         ...state,
-        myList: [...state.myList, action.payload]
-      }
+        myList: [...state.myList, action.payload],
+      };
     case 'DELETE_FAVORITE':
       return {
         ...state,
-        myList: state.myList.filter(items => items.id !== action.payload)
-      }
+        myList: state.myList.filter(items => items.id !== action.payload),
+      };
     case 'LOGIN_REQUEST':
       return {
         ...state,
         user: action.payload,
-      }
+      };
     case 'LOGOUT_REQUEST':
       return {
         ...state,
         user: action.payload,
-      }
+      };
     case 'REGISTER_REQUEST':
-       return {
-         ...state,
-         user: action.payload
-       }
+      return {
+        ...state,
+        user: action.payload,
+      };
     case 'GET_VIDEO_SOURCE':
       return {
         ...state,
-        playing: state.trends.find(item => item.id === Number(action.payload)) 
-        || state.originals.find(item => item.id === Number(action.payload))
-        || []
+        playing: state.trends.find(item => item.id === Number(action.payload))
+        || state.originals.find(item => item.id === Number(action.payload)) ||
+        [],
+      };
+    case 'SEARCH_VIDEO_SOURCE':
+      if (action.payload === '') {
+        return {
+          ...state,
+          search: [],
+        };
       }
-    default: 
+      return {
+        ...state,
+        search: state.content.filter(items => items.title.toLowerCase().includes(action.payload.toLowerCase())) ||
+          [],
+      };
+    default:
       return state;
   }
-}
+};
 
 export default reducer;
